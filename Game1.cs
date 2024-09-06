f9d0a8869a96d6cd30264819aa49644412be809d
Rami Maseesa <132995650+RamiMaseesa@users.noreply.github.com>
2024-09-06 17:20:20 +0200

    Update Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -30,11 +30,7 @@ namespace Assignment1
             objects.Add(new Gate(new Vector2(_graphics.PreferredBackBufferWidth / 2, 100), new string[] { "gateclosed", "gateopen" }, "font", this));
 
             objects.Add(new Sword(new Vector2(500, _graphics.PreferredBackBufferHeight / 2), "sword1", "font"));
-            objects.Add(new Sword(new Vector2(600, _graphics.PreferredBackBufferHeight / 2), "sword2", "font"));
-            objects.Add(new Sword(new Vector2(700, _graphics.PreferredBackBufferHeight / 2), "sword3", "font"));
             objects.Add(new Shield(new Vector2(1300, _graphics.PreferredBackBufferHeight / 2), new string[] { "shield1", "shield1back" }, "font"));
-            objects.Add(new Shield(new Vector2(1400, _graphics.PreferredBackBufferHeight / 2), new string[] { "shield2", "shield2back" }, "font"));
-            objects.Add(new Shield(new Vector2(1500, _graphics.PreferredBackBufferHeight / 2), new string[] { "shield3", "shield3back" }, "font"));
             objects.Add(new Bow(new Vector2(_graphics.PreferredBackBufferWidth / 2, 800), "bow", "font", "arrow"));
 
             objects.Add(new Player(new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2),
