384416332ce351e50bfe13dc19b2e90057617674
Rami Maseesa <132995650+RamiMaseesa@users.noreply.github.com>
2024-09-10 12:16:00 +0200

    fixed sprite bug with depth

--- Scripts/Assignment2/ButtonBase.cs
+++ Scripts/Assignment2/ButtonBase.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using TopDownGame.Scripts.Assignment1;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace TopDownGame.Scripts.Assignment2
@@ -12,6 +14,9 @@ namespace TopDownGame.Scripts.Assignment2
 
         private ButtonStatus buttonStatus;
         private Vector2 textSize;
+        private Rectangle mouseCollider;
+        private MouseState mState;
+        private MouseState previousMState;
         private SpriteFont font;
         public ButtonBase(Vector2 position, string path, string fontPath, string text) : base(position, path)
         {
@@ -22,7 +27,7 @@ namespace TopDownGame.Scripts.Assignment2
         protected internal override void Initialize(GraphicsDeviceManager graphics)
         {
             base.Initialize(graphics);
-            depth = 1f;
+            depth = .99f;
             buttonStatus = ButtonStatus.Normal;
         }
 
@@ -36,14 +41,47 @@ namespace TopDownGame.Scripts.Assignment2
         protected internal override void Update(GameTime gameTime, GraphicsDeviceManager graphics)
         {
             base.Update(gameTime, graphics);
+            mState = Mouse.GetState();
+            mouseCollider = new Rectangle(mState.X + sprite.Width / 2, mState.Y + sprite.Height / 2, 1, 1);
 
+            UpdateButtonColor();
+            ChangeButtonState();
+
+            previousMState = mState;
         }
 
         protected internal override void Draw(SpriteBatch spriteBatch)
         {
             base.Draw(spriteBatch);
 
-            spriteBatch.DrawString(font, text, new Vector2(position.X - textSize.X / 2 + 5, position.Y - 23), Color.WhiteSmoke, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+            spriteBatch.DrawString(font, text, new Vector2(position.X - textSize.X / 2 + 5, position.Y - 23), color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+        }
+
+        private void UpdateButtonColor()
+        {
+            // change button color
+            if (buttonStatus == ButtonStatus.Normal) color = Color.White;
+            else if (buttonStatus == ButtonStatus.Hovered) color = Color.Gray;
+            else if (buttonStatus == ButtonStatus.Pressed) color = Color.Black;
+        }
+
+        private void ChangeButtonState()
+        {
+
+            if (collider.Contains(mouseCollider))
+            {
+                buttonStatus = ButtonStatus.Hovered;
+            }
+            else
+            {
+                buttonStatus = ButtonStatus.Normal;
+            }
+
+            // button is clicked if allready hovering and when button is released
+            if (buttonStatus == ButtonStatus.Hovered && mState.LeftButton == ButtonState.Released && previousMState.LeftButton == ButtonState.Pressed)
+            {
+                buttonStatus = ButtonStatus.Pressed;
+            }
         }
     }
 }
