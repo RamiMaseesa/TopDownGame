39338d2ec197dfa8056b18a721b1a40e7448e01b
Rami Maseesa <132995650+RamiMaseesa@users.noreply.github.com>
2024-09-23 10:29:07 +0200

    started work on enemy states

---
+++ Scripts/Assignment4/Objects/EnemyClasses/PatrolState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDownGame.Scripts.Assignment4.Objects.EnemyClasses
+{
+    internal class PatrolState
+    {
+    }
+}
