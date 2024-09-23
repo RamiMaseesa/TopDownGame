24f6bf63b549cae049a74e36e209751d688fc5b3
Rami Maseesa <132995650+RamiMaseesa@users.noreply.github.com>
2024-09-23 10:22:01 +0200

    added enemy classes

---
+++ Scripts/Assignment4/Objects/EnemyClasses/Enemies/DragonEnemy.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopDownGame.Scripts.Assignment4.HelperClass;
+
+namespace TopDownGame.Scripts.Assignment4.Objects.EnemyClasses.Enemies
+{
+    internal class DragonEnemy : GameObject
+    {
+        public DragonEnemy(Vector2 position, string path) : base(position, path)
+        {
+
+        }
+    }
+}
